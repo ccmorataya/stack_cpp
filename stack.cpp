fa3717144580894a49093324542916f5071827d2
Cristian Morataya <cmorataya@redpronet.com>
2018-02-16 08:10:03 -0600

    Add stack validations; improve the menu; handle the loop menu with return

--- stack.cpp
+++ stack.cpp
@@ -1,38 +1,66 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-void push(int number){
-    cout << "push: " + number;
+int stack[14];
+int index = 0;
+
+bool stackIsFull()
+{
+    if (index == 14)
+        return true;
+    else
+        return false;
 }
 
-void pop(){
-    cout << "call pop()";
+void push(int number)
+{
+    stack[index] = number;
+    index++;
+    cout << "Ingresado el número " << number << endl;
 }
 
-void showStack(){
-    cout << "call showStack()";
+void pop()
+{
+    cout << "call pop()" << endl;
 }
 
-int main(){
-    cout << "Simulación de pila:\n";
-    cout << "Elige una opción:\n";
-    cout << "1. Push\n";
-    cout << "2. Pop\n";
-    cout << "3. Mostrar pila\n";
-    cout << "4. Salir\n";
-    int option = -1;
-    int number;
-    cin >> option;
+void showStack()
+{
+    cout << "call showStack()" << endl;
+}
 
-    if (option == 1) {
-        cout << "Ingresa un número entero";
-        cin >> number;
-        push(number);
+int main()
+{
+    bool desa = false;
+
+    if (!desa) {
+        while (true) {
+            cout << "Simulación de pila:\n"
+                 << endl;
+            cout << "Elige una opción:" << endl;
+            cout << "1. Push" << endl;
+            cout << "2. Pop" << endl;
+            cout << "3. Mostrar pila" << endl;
+            cout << "4. Salir" << endl;
+            int option = -1;
+            int number;
+            cin >> option;
+
+            if (option == 1) {
+                if (!stackIsFull()) {
+                    cout << "Ingresa un número entero" << endl;
+                    cin >> number;
+                    push(number);
+                }
+            } else if (option == 2)
+                pop();
+            else if (option == 3)
+                showStack();
+            else if (option == 4)
+                return 0;
+        }
+    } else {
+        cout << (index);
     }
-    else if (option == 2)
-        pop();
-    else if (option == 3)
-        showStack();
 }
-
