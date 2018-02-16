d8389edbb71b79cbe9d880e3e1ae248600402797
Cristian Morataya <cris.morataya@gmail.com>
2018-02-15 23:47:41 -0600

    Initial Commit

---
+++ stack.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Simulación de pila:\n";
+    cout << "Elige una opción:\n";
+    cout << "1. Push\n";
+    cout << "2. Pop\n";
+    cout << "3. Mostrar pila\n";
+    cout << "4. Salir\n";
+    int option = -1;
+    cin >> option;
+}
