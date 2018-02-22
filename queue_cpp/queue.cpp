903d59ba218e43885289bd53ea72cfac1ce04c28
Cristian Morataya <cris.morataya@gmail.com>
2018-02-21 19:15:07 -0600

    Format code and add cycle

--- queue_cpp/queue.cpp
+++ queue_cpp/queue.cpp
@@ -12,18 +12,22 @@ using namespace std;
 
 const int structLength = 14;
 
-struct customer{
+struct customer {
     int value;
     int ticket;
 } customers[structLength];
 
-int main(){
-    cout << "=========================================" << endl;
-    cout << "\tMenú principal" << endl;
-    cout << "=========================================" << endl;
-    cout << "Elige una opción:" << endl;
-    cout << "1. Ingresar un cliente" << endl;
-    cout << "2. Atender a cliente" << endl;
-    cout << "3. Mostrar cola" << endl;
-    cout << "4. Salir" << endl;
+int main()
+{
+    int option = -1;
+    while (true) {
+        cout << "=========================================" << endl;
+        cout << "\tMenú principal" << endl;
+        cout << "=========================================" << endl;
+        cout << "Elige una opción:" << endl;
+        cout << "1. Ingresar un cliente" << endl;
+        cout << "2. Atender a cliente" << endl;
+        cout << "3. Mostrar cola" << endl;
+        cout << "4. Salir" << endl;
+    }
 }
