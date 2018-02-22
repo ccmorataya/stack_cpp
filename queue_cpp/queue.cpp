cb83079b4cbdb2366351bb873a514915e4f96be5
Cristian Morataya <cris.morataya@gmail.com>
2018-02-21 18:56:41 -0600

    Add cpp file to handle queue

---
+++ queue_cpp/queue.cpp
@@ -0,0 +1,19 @@
+//  Cristian Baudilio Morataya Arriola
+//  //  5990-11-3777
+//  //  Programación 3
+//  //  Sección "C"
+//  //  Año 2018
+//  //  Universidad Mariano Gálvez de Guatemala
+//  //  Sede Chinautla
+
+#include <iostream>
+
+using namespace std;
+
+struct customer{
+    int value;
+    int ticket;
+} customers[0];
+
+int main(){
+}
