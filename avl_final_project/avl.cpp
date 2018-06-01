9a10a0c3bbd982cc277d2d3368ce78462ea8e815
Cristian Morataya <cris.morataya@gmail.com>
2018-06-01 07:09:39 -0600

    Format code

--- avl_final_project/avl.cpp
+++ avl_final_project/avl.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 typedef struct AVL{
-        int dato, FB; // FB es la altura del subarbol izquierdo menos la altura del subarbol derecho
-            AVL *izq, *der;
-                bool borrado;
-
+    int dato, FB; // FB es la altura del subarbol izquierdo menos la altura del subarbol derecho
+    AVL *izq, *der;
+    bool borrado;
 } AVL;
 
 void rotarLL(AVL* &A){ //precond: el árbol necesita una rotacion LL
@@ -38,135 +37,116 @@ void rotarRLalter(AVL* &A){ //precond: el árbol necesita una rotacion RL
 }
 
 AVL* Crear(){
-        return NULL;
-
+    return NULL;
 }
 
 void Insert(int n, bool &aumento, AVL* &A){
     if (A == NULL){
-                A = new AVL;
-                        A->dato = n;
-                                A->FB = 0;
-                                        A->izq = NULL;
-                                                A->der = NULL;
-                                                        aumento = true;
-                                                                A->borrado = false;
-                                                                    
-    }else{
-                if (n < A->dato){           
-                            Insert(n, aumento, A->izq);         
-                            if (aumento){
-                                switch (A->FB){
-                                    case -1:{
-                                                                        A->FB = 0;
-                                                                                                aumento = false;
-                                                                                                                        break;
-                                                                                                                                            
-                                            }
-                                    case 0:{
-                                                                    A->FB = 1;
-                                                                                            aumento = true;
-                                                                                                                    break;
-                                                                                                                                        
-                                           }
-                                    case 1:{
-                                                                    if (A->izq->FB == 1){ // Si es 1 necesita una rotacion LL si es -1 necesita una rotacion LR
-                                                                                                rotarLL(A);
-                                                                    }else{
-                                                                                                    rotarLRalter(A);
-                                                                                                                            
-                                                                    }
-                                                                                            aumento = false;
-                                                                                                                    break;
-                                                                                                                                        
-                                           }
-                                                        
-                                }
-                                            
-                            }
-                }else{
-                                Insert(n, aumento, A->der);         
-                                if (aumento){
-                                    switch (A->FB){
-                                        case -1:{
-                                                                            if (A->der->FB == 1){ // Si es 1 necesita una rotacion RL si es -1 necesita una rotacion RR
-                                                                                                        rotarRLalter(A);
-                                                                            }else{
-                                                                                                            rotarRR(A);
-                                                                                                                                    
-                                                                            }
-                                                                                                    aumento = false;                        
-                                                                                                                            break;
-                                                                                                                                                
-                                                }
-                                        case 0:{
-                                                                        A->FB = -1;
-                                                                                                aumento = true;
-                                                                                                                        break;
-                                                                                                                                            
-                                               }
-                                        case 1:{
-                                                                        A->FB = 0;
-                                                                                                aumento = false;
-                                                                                                                        break;
-                                                                                                                                            
-                                               }
-                                                            
-                                    }
-                                                
-                                }
-                                        
-                }
-                    
+        A = new AVL;
+        A->dato = n;
+        A->FB = 0;
+        A->izq = NULL;
+        A->der = NULL;
+        aumento = true;
+        A->borrado = false;
     }
-
+    else {
+        if (n < A->dato){           
+            Insert(n, aumento, A->izq);         
+            if (aumento){
+                switch (A->FB){
+                    case -1:{
+                        A->FB = 0;
+                        aumento = false;
+                        break;
+                    }
+                    case 0:{
+                        A->FB = 1;
+                        aumento = true;
+                        break;
+                    }
+                    case 1:{
+                        if (A->izq->FB == 1){ // Si es 1 necesita una rotacion LL si es -1 necesita una rotacion LR
+                            rotarLL(A);
+                        }
+                        else {
+                            rotarLRalter(A);
+                        }
+                        aumento = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else {
+            Insert(n, aumento, A->der);         
+            if (aumento){
+                switch (A->FB){
+                    case -1:{
+                        if (A->der->FB == 1){ // Si es 1 necesita una rotacion RL si es -1 necesita una rotacion RR
+                            rotarRLalter(A);
+                        }
+                        else {
+                            rotarRR(A);
+                        }
+                            aumento = false;                        
+                        break;
+                    }
+                    case 0:{
+                        A->FB = -1;
+                        aumento = true;
+                        break;
+                    }
+                    case 1:{
+                        A->FB = 0;
+                        aumento = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
 
 void Insertar(AVL* &A, int n){
-        bool aumento;
-            Insert(n, aumento, A);
-
+    bool aumento;
+    Insert(n, aumento, A);
 }
 
 bool EsVacio(AVL* A){
-        return A == NULL;
-
+    return A == NULL;
 }
 
 bool Pertenece(AVL* A, int n){
     if (A == NULL){
-                return false;
-                    
-    }else{
+        return false;
+    }
+    else {
         if (A->dato == n){
             if (A->borrado){
-                                return false;
-                                            
-            }else{
-                                return true;
-                                            
+                return false;
             }
-                    
-        }else if (n < A->dato){     
-                    return Pertenece(A->izq, n);
-        }else{
-                        return Pertenece(A->der, n);
-                                
+            else {
+                return true;
+            }
+        }
+        else if (n < A->dato){     
+            return Pertenece(A->izq, n);
+        }
+        else {
+            return Pertenece(A->der, n);
         }       
-            
     }
-
 }
 
 void Borrar(AVL* &A, int n){
     if (A->dato == n){
-                A->borrado = true;
-                    
-    }else if (n < A->dato){     
-            Borrar(A->izq, n);
-    }else{
-                Borrar(A->der, n);
-                    
+        A->borrado = true;
     }
-
+    else if (n < A->dato){     
+        Borrar(A->izq, n);
+    }
+    else {
+        Borrar(A->der, n);
+    }
 }
